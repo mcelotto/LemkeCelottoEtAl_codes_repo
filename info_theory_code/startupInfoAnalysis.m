98e9b6d92fc49e11d642b0534a57d41a77214871
mcelotto <cel895@gmail.com>
2024-02-22 17:27:28 +0100

    updated startupInfoAnalysis

--- info_theory_code/startupInfoAnalysis.m
+++ info_theory_code/startupInfoAnalysis.m
@@ -1,10 +1,8 @@
 % setup PID
 % path to BROJA2_PID package
-% BROJA_2PID_path = '/$BROJA_2PID_PATH';
-BROJA_2PID_path = 'C:\Users\mcelotto\Desktop\Neural Computation\PhD\Lemke\Reach_to_grasp_project\Scripts\code_from_Stefan_220224\BROJA_2PID-master';
+BROJA_2PID_path = '/$BROJA_2PID_PATH';
 % path to PIDPyInterface
-%PIDPythonInterface_path = '$PIDPyInterface_PATH';
-PIDPythonInterface_path = 'C:\Users\mcelotto\Desktop\Neural Computation\PhD\Sur_project\scripts\github_sync\LemkeCelottoEtAl_codes_repo\info_theory_code\PIDPyInterface';
+PIDPythonInterface_path = '$PIDPyInterface_PATH';
 insert(py.sys.path, int64(0), PIDPythonInterface_path);
 insert(py.sys.path, int64(0), BROJA_2PID_path);
 
